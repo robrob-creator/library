98565504891ecf84af1f8d2abd70a84b3c77f0a4
robert-moonshot <rob.moonshot@gmail.com>
2022-11-09 17:23:35 +0800

    update

--- src/components/atoms/Text.tsx
+++ src/components/atoms/Text.tsx
@@ -1,20 +1,19 @@
 /* eslint-disable prettier/prettier */
 /* eslint-disable react/no-unknown-property */
-
-import * as React from 'react';
-import { Size } from '../types';
+import { Size } from "../types";
 
 export interface TextProps {
   text?: string;
   color: string;
   size?: Size;
+  className: string;
 }
 
-export const Text = ({ color = 'white', text }: TextProps) => {
+export const Text = ({ color = "white", text }: TextProps) => {
   return (
     <p
       className={`text-xl tracking-wider text-${color}`}
-      style={{ width: '432px', height: '143px' }}
+      style={{ width: "432px", height: "143px" }}
     >
       {text && text}
     </p>
@@ -25,7 +24,7 @@ export const HeaderText = ({ color, text }: TextProps) => {
   return (
     <p
       className={`text-4xl font-bold tracking-wider text-${color}`}
-      style={{ width: '432px' }}
+      style={{ width: "432px" }}
     >
       {text && text}
     </p>
