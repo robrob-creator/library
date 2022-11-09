97f867afe169ed92c4674851564d2add4924e275
robert-moonshot <rob.moonshot@gmail.com>
2022-11-09 18:26:46 +0800

    fix-build-error

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,12 +3,9 @@ module.exports = {
   mode: "jit",
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-<<<<<<< HEAD
     fontFamily: {
       Poppins: ["Poppins", "sans-serif"],
     },
-=======
->>>>>>> origin
     extend: {},
   },
   plugins: [],
