bc80be1489a65e97cf950ccf7ca75926864dd650
robert-moonshot <rob.moonshot@gmail.com>
2022-12-01 12:41:34 +0800

    arrow-right-icon

--- src/components/atoms/icons.tsx
+++ src/components/atoms/icons.tsx
@@ -604,3 +604,29 @@ export const AddCirlceIcon = ({
     </svg>
   );
 };
+
+export const ArrowRightIcon = ({
+  className,
+  color = "#111827",
+  width = "20",
+  height = "16",
+}: LogoProps) => {
+  return (
+    <svg
+      width={width}
+      height={height}
+      className={className}
+      viewBox="0 0 20 16"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M12 1L19 8M19 8L12 15M19 8L1 8"
+        stroke={color}
+        strokeWidth="2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+    </svg>
+  );
+};
