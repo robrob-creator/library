435fce6e56fc2c508ceea49f6a1979b8eff1c974
robert-moonshot <rob.moonshot@gmail.com>
2022-12-15 12:56:08 +0800

    feature/polished-list

--- src/components/molecules/list.tsx
+++ src/components/molecules/list.tsx
@@ -3,7 +3,6 @@
 import React from "react";
 import { Text } from "../atoms/Text";
 import { Layout } from "../types";
-
 export interface ListProps {
   data?: {
     label?: React.ReactNode;
@@ -13,58 +12,64 @@ export interface ListProps {
     leftIcon?: React.ReactNode;
     rightIcon?: React.ReactNode;
   }[];
-  alternate?: boolean;
+  className?: string;
   layout?: Layout;
+  alternate?: boolean;
 }
 
 export const List = <PROPS extends ListProps>({
   data,
   alternate,
   layout = "vertical",
+  className,
 }: PROPS): JSX.Element => {
   return (
-    <div
-      className={`inline-flex ${
-        layout == "vertical" ? "flex-col" : "sm:flex-row flex-col"
-      }  items-start justify-start w-full gap-2`}
-    >
-      {data &&
-        data?.map((item, index) => {
-          return (
-            <div
-              className={`${
-                item?.className
-              } inline-flex items-start justify-start w-full${
-                alternate && index % 2 != 0 && "bg-gray-200"
-              }`}
-              style={{ height: 50 }}
-            >
-              <div
-                className="flex items-center justify-start pl-5 w-full"
-                style={{ height: 50 }}
-              >
-                {item?.leftIcon}
-                <Text size="xl" color="gray-600" fontStyle="medium">
-                  {item?.label}
-                </Text>
-              </div>
-              <div
-                className="flex items-center justify-end pr-5 gap-2 w-full"
-                style={{ height: 50 }}
-              >
-                <Text
-                  size="xl"
-                  color="gray-600"
-                  fontStyle="medium"
-                  className="text-right"
+    <div>
+      <div
+        className={`${className} inline-flex ${
+          layout == "vertical" ? "flex-col" : "flex-row"
+        } items-start justify-start w-full gap-2`}
+      >
+        {data &&
+          data?.map((item, index) => {
+            if (!item.content)
+              return (
+                <div
+                  className={`${
+                    item?.className
+                  } inline-flex items-start justify-start w-full ${
+                    alternate && index % 2 != 0 && "bg-gray-200"
+                  }`}
+                  style={{ height: 50 }}
                 >
-                  {item?.value}
-                </Text>
-                {item.rightIcon}
-              </div>
-            </div>
-          );
-        })}
+                  <div
+                    className="flex items-center justify-start pl-5 w-full"
+                    style={{ height: 50 }}
+                  >
+                    {item?.leftIcon}
+                    <Text size="xl" color="gray-600" fontStyle="medium">
+                      {item?.label}
+                    </Text>
+                  </div>
+                  <div
+                    className="flex items-center justify-end pr-5 gap-2 w-full"
+                    style={{ height: 50 }}
+                  >
+                    <Text
+                      size="xl"
+                      color="gray-600"
+                      fontStyle="medium"
+                      className="text-right"
+                    >
+                      {item?.value}
+                    </Text>
+                    {item.rightIcon}
+                  </div>
+                </div>
+              );
+            return item.content;
+          })}
+      </div>
     </div>
   );
 };
--- src/stories/List.stories.tsx
+++ src/stories/List.stories.tsx
@@ -26,79 +26,8 @@ const Template: Story<ListProps> = (args) => <List {...args} />;
 export const Default = Template.bind({});
 
 Default.args = {
-  layout: "horizontal",
   data: [
-    {
-      value: (
-        <div className="inline-flex justify-items-center px-4 sm:px-24 flex-col  sm:space-y-96 border-r-2 border-gray-400">
-          <div className="inline-flex flex-col items-center justify-end sm:w-16 sm:h-24">
-            <p className="w-full text-xs sm:text-base font-bold  text-center text-gray-700">
-              Ratings
-            </p>
-            <p className="text-2xl sm:text-3xl font-semibold leading-10 text-center text-gray-700">
-              4.5
-            </p>
-            <div className="inline-flex space-x-0.5 items-start justify-start">
-              <img
-                className="w-1/6 h-full rounded-full"
-                src="https://via.placeholder.com/10x10"
-              />
-              <img
-                className="w-1/6 h-full rounded-full"
-                src="https://via.placeholder.com/10x10"
-              />
-              <img
-                className="w-1/6 h-full rounded-full"
-                src="https://via.placeholder.com/10x10"
-              />
-              <img
-                className="w-1/6 h-full rounded-full"
-                src="https://via.placeholder.com/10x10"
-              />
-              <img
-                className="w-1/6 h-full rounded-full"
-                src="https://via.placeholder.com/10x10"
-              />
-            </div>
-          </div>
-        </div>
-      ),
-    },
-    {
-      value: (
-        <div className="inline-flex justify-items-center px-4 sm:px-24 flex-col  sm:space-y-96 border-r-2 border-gray-400">
-          <div className="inline-flex flex-col items-center justify-end sm:w-16 sm:h-24">
-            <p className="w-full text-xs sm:text-base font-bold  text-center text-gray-700">
-              Ratings
-            </p>
-            <p className="text-2xl sm:text-3xl font-semibold leading-10 text-center text-gray-700">
-              4.5
-            </p>
-            <div className="inline-flex space-x-0.5 items-start justify-start">
-              <img
-                className="w-1/6 h-full rounded-full"
-                src="https://via.placeholder.com/10x10"
-              />
-              <img
-                className="w-1/6 h-full rounded-full"
-                src="https://via.placeholder.com/10x10"
-              />
-              <img
-                className="w-1/6 h-full rounded-full"
-                src="https://via.placeholder.com/10x10"
-              />
-              <img
-                className="w-1/6 h-full rounded-full"
-                src="https://via.placeholder.com/10x10"
-              />
-              <img
-                className="w-1/6 h-full rounded-full"
-                src="https://via.placeholder.com/10x10"
-              />
-            </div>
-          </div>
-        </div>
-      ),
-    },
+    { label: "string1", value: "string1", rightIcon: <CogIcon /> },
+    { label: "string1", value: "string2" },
   ],
 };
